7736ce6f6fd8333a90da90ba412fe4795643f4fc
jylux <kalejaiye.gbenga@gmail.com>
2019-09-18 13:11:51 +0100

    Modified User Interface

--- js/signup.js
+++ js/signup.js
@@ -1,46 +1,62 @@
-$(function() {
-    $("#submit").click(()=>{
-    var firstname = $("#firstname").val();
-    var lastname = $("#lastname").val();
-    var email  = $("#emailId").val();
-    var password= $("#mypassword").val();
-  
-    $.ajax({
-      type: "GET",
-      url: "http://localhost:3000/users",
-      dataType: "json"
-    }).done((data)=>{
-      console.log(data);
-      let check = 0;
-      for (let i of data){
-        if (i.email == email) {
-          alert(`Sorry, User already exists`);
-          check = 1;
-        }
-      }
-      if (check == 0) {
-        if (firstname == null || firstname == "", email == null || email== "", password == null || password == "") {
-          alert("fields cannot be blank");
-          return false;
-        }
-          $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/users",
-            data: {
-              firstname: firstname,
-              lastname: lastname,
-              email: email,
-              password: password,
-              balance: 0,
-              transactions: "[]"
-            }
-          }).done((data)=>{
-            console.log(data);
-            alert('welcome');
-            // window.location.href = "user-dashboard.html";
-            $(location).attr("href", "user-login.html");
-          });
-        }
+$(document).ready(function() {
+    //Over sabi Effect
+    $('.registerBtn').click(function() {
+      $('.regForm').fadeIn();
     });
-  });
-  });
+    $('.closeBtn').click(function() {
+      $('.regForm').fadeOut();
+    });
+    $('.loginBtn').click(function() {
+      $('.loginForm').fadeIn();
+    });
+    $('.closeLoginBtn').click(function() {
+      $('.loginForm').fadeOut();
+    });
+    //Registration Function
+    $('.signUpBtn').click(function(event) {
+      event.preventDefault();
+      const firstname = $('#firstname').val();
+      const lastname = $('#lastname').val();
+      const password = $('#password').val();
+      const email = $('#email').val();
+      //Check if user input is empty
+      if (!fullname || !username || !password || !email) {
+        $('.regMessage').html('Kindly fill in all fields');
+        return;
+      }
+      //Make get request to check if the user already exist
+      $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/users?email=${email}`,
+        data: {
+          email,
+        },
+        beforeSend: function() {
+          $('.regMessage').html('Loading....');
+        },
+        success: function(response) {
+          if (response.length) {
+            $('.regMessage').html('User already exist');
+          } else {
+            //Submit the user data if the user does not exist
+            $.ajax({
+              method: 'POST',
+              url: 'http://localhost:3000/users',
+              data: {
+                firstname,
+                lastname,
+                email,
+                password,
+              },
+              beforeSend: function() {
+                $('.regMessage').html('Loading....');
+              },
+              success: function() {
+                $('.regMessage').html('Registration Successfull');
+              },
+            });
+          }
+        },
+      });
+    });
+});
