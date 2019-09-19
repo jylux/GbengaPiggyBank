0fa21feeefba91b58d6c77560145362e74a9fb5e
jylux <kalejaiye.gbenga@gmail.com>
2019-09-19 06:40:38 +0100

    improved deposit.js file

--- js/deposit.js
+++ js/deposit.js
@@ -1,51 +1,75 @@
 $(document).ready(function() {
-$('#signUpBtn').click(function(event) {
-    event.preventDefault();
-    const firstname = $('#firstname').val();
-    const lastname = $('#lastname').val();
-    const username = $('#username').val();
-    const password = $('#password').val();
-    const email = $('#email').val();
-    //Check if user input is empty
-    if (!fullname || !username || !password || !email) {
-      $('.regMessage').html('Kindly fill in all fields');
-      return;
+  $(function () {
+    let currentUser = localStorage.getItem("email");
+    console.log(currentUser);
+  
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    
+    if (day < 10) {
+      day = "0" + day;
     }
-    //Make get request to check if the user already exist
-    $.ajax({
-      method: 'GET',
-      url: `http://localhost:3000/users?email=${email}`,
-      data: {
-        email,
-      },
-      beforeSend: function() {
-        $('.regMessage').html('Loading....');
-      },
-      success: function(response) {
-        if (response.length) {
-          $('.regMessage').html('User already exist');
-        } else {
-          //Submit the user data if the user does not exist
+    if(month <10){
+      month = "0" + month;
+    }
+    today = day + "-" + month + "-" + year;
+    console.log(today);
+    
+    $("#submitAmount").click(function addTransactions(deposit) {
+      let obj = {};
+      let amount = parseInt($("#deposit").val());
+      let url =  `http://localhost:3000/users/?email=${currentUser}`;
+      let transaction_date = today;
+      if (!amount || amont < 0) {
+        alert("Invalid amount");
+        return false;
+      }
+      else {
+        $.ajax({
+          method: "GET",
+          url: url,
+          data: {json,
+          }, beforeSend: function() {
+          $('.regMessage').html('Loading....');
+          },
+          success: function(response) {
+          var transArray = response["transactions"];
+          var newArr = JSON.parse(transArray);
+          var id = newArr.length + 1;
+          $("#deposit").each(function(){
+            obj['id'] = id,
+            obj['deposit'] = amount,
+            obj['transaction_date'] = transaction_date;
+          });
+          newArr.push(obj);
+          transArray = JSON.stringify(newArr);
+  
+          if (response.hasOwnProperty("balance")) {
+          var newBalance = 0;
+          var newDeposit = parseInt(response.balance) + amount;
+          newBalance += newDeposit;
+          }
+          else{
+            alert("Error");
+          }
           $.ajax({
-            method: 'POST',
-            url: 'http://localhost:3000/users',
-            data: {
-              firstname,
-              lastname,
-              username,
-              email,
-              password,
-            },
-            beforeSend: function() {
-              $('.regMessage').html('Loading....');
-            },
-            success: function() {
-              $('.regMessage').html('Registration Successfull');
-              windows.location.assign('login.html')
-            },
+          method: "PUT",
+          url: `http://localhost:3000/users/${response.id}`,
+          data: {
+            newBalance,
+            transactionsArray
+          },
+           beforeSend: function() {
+            $('.regMessage').html('Loading....');
+           },
+            success: function(response){
+           $('#regMessage').html('Successful Deposit');
+          $('#result').html("Your Balance is: " + newBalance)
+          });
           });
         }
-      },
     });
   });
-});
+}); 
